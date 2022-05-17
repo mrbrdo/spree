ecd3eeb5be1321d0cebaa2c014c7e4b642a44b5e
mrbrdo <mrbrdo@gmail.com>
2022-05-17 14:09:08 +0200

    always save the last used ship/bill address as default for User
    best solution until there is a proper address book where user can choose address from dropdown and set the default address

--- core/app/models/spree/order/address_book.rb
+++ core/app/models/spree/order/address_book.rb
@@ -30,7 +30,7 @@ module Spree
 
       def bill_address_attributes=(attributes)
         self.bill_address = update_or_create_address(attributes)
-        user.class.unscoped.where(id: user).update_all(bill_address_id: bill_address.id) if user && user.bill_address.nil?
+        user.class.unscoped.where(id: user).update_all(bill_address_id: bill_address.id) if user
       end
 
       def ship_address_id=(id)
@@ -50,7 +50,7 @@ module Spree
           else
             update_or_create_address(attributes)
           end
-        user.class.unscoped.where(id: user).update_all(ship_address_id: ship_address&.id) if user && user.ship_address.nil?
+        user.class.unscoped.where(id: user).update_all(ship_address_id: ship_address&.id) if user
       end
 
       private
