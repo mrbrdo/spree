cee337b9ad82479976ee61a9306bd8944220c232
mrbrdo <mrbrdo@gmail.com>
2022-11-22 19:04:42 +0100

    remove redundant query (2 -> 1)
    also works correctly in case multiple categories would have is_default: true

--- core/app/models/spree/tax_category.rb
+++ core/app/models/spree/tax_category.rb
@@ -18,8 +18,8 @@ module Spree
     def set_default_category
       # set existing default tax category to false if this one has been marked as default
 
-      if is_default && tax_category = self.class.where(is_default: true).where.not(id: id).first
-        tax_category.update_columns(is_default: false, updated_at: Time.current)
+      if is_default
+        self.class.where(is_default: true).where.not(id: id).update_all(is_default: false, updated_at: Time.current)
       end
     end
   end
